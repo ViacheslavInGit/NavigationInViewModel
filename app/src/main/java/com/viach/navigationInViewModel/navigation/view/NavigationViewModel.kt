e1c61b2f2599e8e35dbfbc28bb4cc7f022edbcda
bilyi <vyacheslav.bilyi@nixsolutions.com>
2020-03-11 11:14:58 +0200

    small fixies

--- app/src/main/java/com/viach/navigationInViewModel/navigation/view/NavigationViewModel.kt
+++ app/src/main/java/com/viach/navigationInViewModel/navigation/view/NavigationViewModel.kt
@@ -1,14 +1,14 @@
 package com.viach.navigationInViewModel.navigation.view
 
+import androidx.lifecycle.ViewModel
 import com.viach.navigationInViewModel.core.SingleLiveEvent
 import com.viach.navigationInViewModel.navigation.Screen
 import com.viach.navigationInViewModel.navigation.command.Back
 import com.viach.navigationInViewModel.navigation.command.NavigationCommand
 import com.viach.navigationInViewModel.navigation.command.To
-import com.viach.navigationInViewModel.view.BaseViewModel
 import javax.inject.Inject
 
-open class NavigationViewModel @Inject constructor() : BaseViewModel() {
+open class NavigationViewModel @Inject constructor() : ViewModel() {
 
     val navigationCommands = SingleLiveEvent<NavigationCommand>()
 
